ef7d1514463d2157408891d0b4523d32f549e077
Rizki Kurnia Hapsari <rizkikurnia@femaledaily.com>
2025-05-17 21:48:06 +0700

    move get and refresh token to utils

--- src/scenarios/post-notif.js
+++ src/scenarios/post-notif.js
@@ -1,28 +1,16 @@
 import { sendPost } from "../../utils/http-helpers.js";
-import { sendGet } from "../../utils/http-helpers.js";
-import { notifBody, bodyToken } from "../../utils/body-helpers.js";
+import { notifBody } from "../../utils/body-helpers.js";
+import { getToken } from "../../utils/token-helpers.js";
 import { baseUrl } from "../baseUrl/url.js";
-import { headerNotif, headerRefresh } from "../headers/header.js";
+import { headerNotif } from "../headers/header.js";
 
-export function postNotif(){
-    const localUrl = baseUrl('local')
-    
-    // get token
-    const getTokenResponse = sendGet(`${localUrl}/get-token`);
-    const tokenData = getTokenResponse.json()
-    const token = tokenData.token
-    
-    // refresh token
-    const bodyRefresh = bodyToken(token)
-    const ref = sendPost(`${url}/v1/authorization/refresh-token`, bodyRefresh, headerRefresh())
-    
+export function postNotif(){    
     const environment = __ENV.env || 'dev'
     const url = baseUrl(environment)
-    const forHeader = headerNotif(token)
-
+    const forHeader = headerNotif(getToken())
     const body = notifBody()
 
     const res = sendPost(`${url}/fdn-client-notification/v3/notification`, body, forHeader)
-    console.log('Get detail postman response: ', res.json())
+    console.log('Get detail response: ', res.json())
     return res
 }
