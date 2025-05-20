f175a2b5ec65b1f3b4507134fe3e055dc4734a69
Rizki Kurnia Hapsari <rizkikurnia@femaledaily.com>
2025-05-20 08:35:34 +0700

    change target vu and body placeholder product review

--- utils/body-helpers.js
+++ utils/body-helpers.js
@@ -1,15 +1,15 @@
 export function notifBody() {
     return {
             placeholders: [
-                    { username: "hengkytest41" },
-                    { category_slug: "eyes" },
-                    { child_category_slug: "eyeliner" },
-                    { brand_slug: "maybelline" },
-                    { product_slug: "hypersharp-liner" }
+                    { username: "QAStressTestDev-Notif" },
+                    { category_slug: "face-1" },
+                    { child_category_slug: "makeup-remover" },
+                    { brand_slug: "bioderma" },
+                    { product_slug: "sensibio-h2o-48" }
                 ],
             type: "Like Review",
-            receiver_user_id: 446890,
-            image: "https://image.femaledaily.com/dyn/210/images/prod-pics/product_1545194985_Maybelline_800x800.jpg"
+            receiver_user_id: 448313,
+            image: "https://image.femaledaily.com/dyn/210/images/prod-pics/product_1537947928_Bioderma_S_800x800.jpg"
         }
     }
 
