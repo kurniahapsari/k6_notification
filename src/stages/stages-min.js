96369a3bbe6dcbd4642b4eed49bfa22d48275e32
Rizki Kurnia Hapsari <rizkikurnia@femaledaily.com>
2025-05-27 10:13:28 +0700

    adjust host canary prod and payload for notification

--- src/stages/stages-min.js
+++ src/stages/stages-min.js
@@ -1,7 +1,13 @@
 export const minUser = {
     stages: [
+<<<<<<< Updated upstream
         {duration: '1s', target: 3},
         {duration: '5s', target: 5},
         {duration: '3s', target: 0}
+=======
+        {duration: '1s', target: 0},
+        {duration: '2s', target: 1},
+        {duration: '2s', target: 0}
+>>>>>>> Stashed changes
     ]
 }
--- utils/body-helpers.js
+++ utils/body-helpers.js
@@ -1,6 +1,23 @@
+// export function notifBody() { // dev
+//     return {
+//             placeholders: [
+//                     { username: "QAStressTestDev-Notif" },
+//                     { category_slug: "face-1" },
+//                     { child_category_slug: "makeup-remover" },
+//                     { brand_slug: "bioderma" },
+//                     { product_slug: "sensibio-h2o-48" }
+//                 ],
+//             type: "Like Review",
+//             receiver_user_id: 448313,
+//             image: "https://image.femaledaily.com/dyn/210/images/prod-pics/product_1537947928_Bioderma_S_800x800.jpg"
+//         }
+//     }
+
+//prod
 export function notifBody() {
     return {
             placeholders: [
+<<<<<<< Updated upstream
                     { username: "hengkytest41" },
                     { category_slug: "eyes" },
                     { child_category_slug: "eyeliner" },
@@ -10,6 +27,17 @@ export function notifBody() {
             type: "Like Review",
             receiver_user_id: 446890,
             image: "https://image.femaledaily.com/dyn/210/images/prod-pics/product_1545194985_Maybelline_800x800.jpg"
+=======
+                    { username: "QAStressTestProd-Notif" },
+                    { category_slug: "oil-1" },
+                    { child_category_slug: "vitamin-serum" },
+                    { brand_slug: "ellips" },
+                    { product_slug: "hair-vitamin-1" }
+                ],
+            type: "Like Review",
+            receiver_user_id: 114313,
+            image: "https://image.femaledaily.com/dyn/210/images/prod-pics/product_1442301703_ellips_800x800.jpg"
+>>>>>>> Stashed changes
         }
     }
 
